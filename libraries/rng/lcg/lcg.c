78e1801a855f60c3a791e222a8a247c44b3d984d
Jake Muff <jake.muff@mac.com>
2021-02-09 21:02:29 +0200

    Zipped libraries

--- libraries/rng/lcg/lcg.c
+++
@@ -1,9 +0,0 @@
-
-double lcg(long long int *seed)
-{
-  static long long int a=69069,c=1,m=4294967296; /* 2^32 */
-  static double rm=4294967296.0;
-
-  *seed=(*seed * a+c)%m;
-  return (double)*seed/rm;
-}
--- libraries/rng/mersennetwister/defs.f90
+++
@@ -1,3 +0,0 @@
-module defs
-  integer,parameter :: rk=selected_real_kind(10,40)
-end module defs
